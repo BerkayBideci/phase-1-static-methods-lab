8dc115ebf44b8b8aa9934f760a10f5f998b2f869
Berkay Bideci <berkaybideci@gmail.com>
2023-05-15 16:38:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(input) {
+    return input.charAt(0).toUpperCase() + input.slice(1)
+  }
+
+  static sanitize(input) {
+    return input.replace(/[^a-zA-Z0-9-' ]/g, '')
+  }
+
+  static titleize(input) {
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const fragmentedInput = input.split(" ");
+    let titleizedInput = [];
+
+    for (let word of fragmentedInput) {
+      if (titleizedInput.length === 0 || !exceptions.includes(word)) {
+        titleizedInput.push(this.capitalize(word));
+      } else {
+        titleizedInput.push(word);
+      }
+    }
+
+    return titleizedInput.join(" ");
+  }
 }
